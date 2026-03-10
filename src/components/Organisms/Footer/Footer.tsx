593e893b16b72e363aa0cee21dc3b9945d52ab91
mattmalicki <mateusz.malicki96@gmail.com>
2026-03-10 17:47:00 +0100

    Added header and footer components.

---
+++ src/components/Organisms/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} BPS Trondheim. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Organisms/Header/Header.tsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <div>Logo</div>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
