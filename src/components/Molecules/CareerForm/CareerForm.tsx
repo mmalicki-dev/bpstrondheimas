de2eb58e0e98acd47bd8360299d2d1d786989484
mattmalicki <mateusz.malicki96@gmail.com>
2026-03-19 17:30:52 +0100

    About us finished.

--- src/components/Molecules/CareerForm/CareerForm.tsx
+++ src/components/Molecules/CareerForm/CareerForm.tsx
@@ -15,7 +15,9 @@ const CareerForm = () => {
     coverLetter: "",
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
@@ -28,11 +30,31 @@ const CareerForm = () => {
       <Input label="Full name" name="fullName" value={form.fullName} onChange={handleChange} />
       <Input label="Email" name="email" type="email" value={form.email} onChange={handleChange} />
       <Input label="Phone" name="phone" type="tel" value={form.phone} onChange={handleChange} />
-      <Input label="Address" name="address" value={form.address} onChange={handleChange} />
-      <Input label="City" name="city" value={form.city} onChange={handleChange} />
-      <Textarea label="Cover letter" name="coverLetter" value={form.coverLetter} onChange={handleChange} />
+      <Input
+        label="Address"
+        name="address"
+        value={form.address}
+        onChange={handleChange}
+      />
+      <Input
+        label="City"
+        name="city"
+        value={form.city}
+        onChange={handleChange}
+      />
+      <Textarea
+        label="Cover letter"
+        name="coverLetter"
+        value={form.coverLetter}
+        onChange={handleChange}
+      />
       <FileInput label="Resume" name="resume" onChange={() => {}} />
-      <FileInput label="Additional files" name="additionalFiles" multiple onChange={() => {}} />
+      <FileInput
+        label="Additional files"
+        name="additionalFiles"
+        multiple
+        onChange={() => {}}
+      />
       <Button label="Submit application" type="submit" />
     </form>
   );
