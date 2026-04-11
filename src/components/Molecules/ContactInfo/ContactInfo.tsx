591bd0d44c08b44e76e487d1be88e912fb49922f
mattmalicki <mateusz.malicki96@gmail.com>
2026-04-11 23:03:56 +0200

    Added organization number to contact info.

--- src/components/Molecules/ContactInfo/ContactInfo.tsx
+++ src/components/Molecules/ContactInfo/ContactInfo.tsx
@@ -7,27 +7,34 @@ interface ContactInfoProps {
 const ContactInfo = ({ full = false }: ContactInfoProps) => {
   return (
     <div className={styles.contactInfo}>
-      {!full && <h2>Contact us</h2>}
+      {!full && <h2 className={styles.contactFooterHeader}>Contact us</h2>}
       {full && (
         <div className={styles.group}>
-          <h3>Address</h3>
-          <p>Eksempelgata 12</p>
-          <p>7010 Trondheim, Norway</p>
+          <h3 className={styles.groupTitle}>Address</h3>
+          <p className={styles.groupTekst}>Eksempelgata 12</p>
+          <p className={styles.groupTekst}>7010 Trondheim, Norway</p>
         </div>
       )}
       <div className={styles.group}>
-        <h3>Phone</h3>
-        <a href="tel:+4712345678">+47 123 45 678</a>
+        <h3 className={styles.groupTitle}>Org. nr</h3>
+        <p className={styles.groupTekst}>123 456 789</p>
       </div>
       <div className={styles.group}>
-        <h3>Email</h3>
-        <a href="mailto:post@bpstrondheim.no">post@bpstrondheim.no</a>
+        <h3 className={styles.groupTitle}>Phone</h3>
+        <a className={styles.groupTekst} href="tel:+4712345678">
+          +47 123 45 678
+        </a>
+      </div>
+      <div className={styles.group}>
+        <h3 className={styles.groupTitle}>Email</h3>
+        <a className={styles.groupTekst} href="mailto:post@bpstrondheim.no">
+          post@bpstrondheim.no
+        </a>
       </div>
       {full && (
         <div className={styles.group}>
-          <h3>Opening hours</h3>
-          <p>Monday – Friday: 07:00 – 16:00</p>
-          <p>Saturday – Sunday: Closed</p>
+          <h3 className={styles.groupTitle}>Opening hours</h3>
+          <p className={styles.groupTekst}>Monday – Friday: 07:00 – 16:00</p>
         </div>
       )}
     </div>
